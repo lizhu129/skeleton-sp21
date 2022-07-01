4dc0a95375bc8e13de2d5a0d5ff50a759da88249
lizhu129 <lizhu129@gmail.com>
2022-07-01 23:23:04 +0200

    lab3 exercise randomizedtest

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,7 +1,7 @@
 package randomizedtest;
 
-import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
+import java.util.Random;
 import static org.junit.Assert.*;
 
 /**
@@ -9,4 +9,59 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+  @Test
+  public void testThreeAddThreeRemove() {
+      AListNoResizing<Integer> a = new AListNoResizing<>();
+      BuggyAList<Integer> b = new BuggyAList<>();
+
+      a.addLast(4);
+      b.addLast(4);
+
+      a.addLast(5);
+      b.addLast(5);
+
+      a.addLast(6);
+      b.addLast(6);
+
+      assertEquals(a.size(), b.size());
+
+      assertEquals(a.removeLast(), b.removeLast());
+      assertEquals(a.removeLast(), b.removeLast());
+      assertEquals(a.removeLast(), b.removeLast());
+
+  }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
+
+        int N = 5000;
+        Random ran = new Random();
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = ran.nextInt(4) + 0;;
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = ran.nextInt(100) + 0;
+                L.addLast(randVal);
+                B.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                assertEquals(L.size(), B.size());
+            } else if (operationNumber == 2) {
+                if(L.size() <= 0 || B.size() <= 0) {
+                    continue;
+                }
+                assertEquals(L.getLast(), B.getLast());
+            } else {
+                if(L.size() <= 0 || B.size() <= 0) {
+                    continue;
+                }
+                assertEquals(L.removeLast(), B.removeLast());
+            }
+
+        }
+
+    }
 }
