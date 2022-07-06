534c5e1c23a9b32c40a9b3a90b7d7e61a0467cf5
lizhu129 <lizhu129@gmail.com>
2022-07-06 15:03:45 +0200

    Finished Lab7 BSTMap

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,226 @@
+package bstmap;
+
+import java.util.*;
+
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private Node root;
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        private int size;
+
+        public Node(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+    }
+
+    public BSTMap() {
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public void clear() {
+        this.root = null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) throw new IllegalArgumentException("key cannot be empty!");
+        return this.get(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(Node x, K key) {
+        if (key == null) throw new IllegalArgumentException("key cannot be empty!");
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x.value;
+        } else if (cmp < 0) {
+            return get(x.left, key);
+        } else {
+            return get(x.right, key);
+        }
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (key == null) throw new IllegalArgumentException("invalid key");
+        if (value == null) {
+            //remove(key);
+            return;
+        }
+        root = put(root, key, value);
+    }
+
+    private Node put(Node x, K key, V value) {
+        if (x == null) {
+            return new Node(key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            x.value = value;
+        } else if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else {
+            x.right = put(x.right, key, value);
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException("Unsupported function");
+     }
+
+    @Override
+    public V remove(K key) {
+        if (key == null) throw new IllegalArgumentException("calls delete() with a null key");
+        V value = get(key);
+        root = remove(root, key);
+        return value;
+    }
+
+    private Node remove(Node x, K key)  {
+        if (x == null) return null;
+
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) {
+            x.left  = remove(x.left,  key);
+        }
+        else if (cmp > 0) {
+            x.right = remove(x.right, key);
+        }
+        else {
+            if (x.right == null) {
+                return x.left;
+            }
+            if (x.left == null) {
+                return x.right;
+            }
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public K min() {
+        if (isEmpty()) throw new NoSuchElementException("calls min() with empty symbol table");
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x;
+        }
+        return min(x.left);
+    }
+
+    public K max() {
+        if (isEmpty()) throw new NoSuchElementException("calls msx() with empty symbol table");
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) {
+            return x;
+        }
+        return max(x.right);
+    }
+
+    public void deleteMin () {
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void deleteMax() {
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node x) {
+        if (x.right == null) return x.left;
+        x.right = deleteMax(x.right);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+
+    @Override
+    public V remove(K key, V value) {
+        if (key == null) throw new IllegalArgumentException("calls delete() with a null key");
+        if (this.get(key).equals(value)) {
+            root = remove(root, key);
+            return value;
+        } else {
+            return null;
+        }
+    }
+
+
+
+    @Override
+    public Iterator<K> iterator() {
+        if (isEmpty()) {
+            return (Iterator<K>) new Queue<K>();
+        }
+        return (Iterator<K>) keys(min(), max());
+    }
+
+    public Iterable<K> keys (K lo, K hi) {
+        if (lo == null) throw new IllegalArgumentException("first argument to keys() is null");
+        if (hi == null) throw new IllegalArgumentException("second argument to keys() is null");
+
+        Queue<K> queue = new Queue<K>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    private void keys(Node x, Queue<K> queue, K lo, K hi) {
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+        if (cmphi > 0) keys(x.right, queue, lo, hi);
+    }
+}
